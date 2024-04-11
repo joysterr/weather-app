5bd757714ac2564f9ae66cdd4a29813d2a6715e9
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-04-11 19:54:03 +0100

    optimise script fetch code and update css styling
    - added favicon
    - updated api fetch script
    - merged css classes to reduce unnecessary complexity
    - further js optimisations

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 const url = import.meta.env.VITE_API || process.env.VITE_API
 
-function getWeatherData() {
-    fetch(url)
-        .then(res => res.json())
-        .then(data => renderWeatherData(data))
-        .catch(err => console.error(err))
+async function getWeatherData() {
+    try {
+        const res = await fetch(url)
+        if (!res.ok) {
+            throw new Error(`Error! Status:  ${res.status}`)
+        }
+        const data = await res.json()
+        renderWeatherData(data)
+    } catch (error) {
+        console.error('Error: ', error.message)
+    }
 }
 
 function renderWeatherData(data) {
-    console.log(data)
-    let location = document.querySelector('#location')
-    let temp = document.querySelector('#temp')
-    let weather = document.querySelector('#weather')
+    const location = document.querySelector('#location')
+    const temp = document.querySelector('#temp')
+    const weather = document.querySelector('#weather')
 
     location.innerHTML = data.name
     temp.innerHTML = Math.round(data.main.temp)
